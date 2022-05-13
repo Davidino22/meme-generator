84097096c454754fc2ad0f43bfc830b5b77ce6eb
David Pecic <david-pecic@gmx.de>
2022-05-13 17:37:36 +0200

    add code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,95 @@
-import React from 'react';
-import Header from './Header';
-import MemeGenerator from './MemeGenerator.js';
+import './App.css';
+import axios from 'axios';
+import FileSaver from 'file-saver';
+import { useEffect, useState } from 'react';
 
-/**
- * Challenge:
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
-export default function App() {
+function App() {
+  const [templates, setTemplates] = useState([]);
+  const [drop, setDrop] = useState('');
+  const [url, setUrl] = useState('');
+  const [toptext, setToptext] = useState('');
+  const [bottomtext, setBottomtext] = useState('');
+  const [image, setImage] = useState(
+    'https://api.memegen.link/images/aag/foo/bar.png',
+  );
+  const saveFile = () => {
+    FileSaver.saveAs(image, 'meme.png');
+  };
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await axios.get('https://api.memegen.link/templates');
+        setTemplates(result.data);
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    })().catch((error) => {
+      console.log(error);
+    });
+  }, []);
   return (
-    <div>
-      <Header />
-      <MemeGenerator />
+    <div className="container">
+      <select
+        onChange={(event) => {
+          setDrop(event.currentTarget.value);
+        }}
+      >
+        {templates.map((template) => (
+          <option key={template.id} value={template.id}>
+            {template.name}
+          </option>
+        ))}
+      </select>
+      <img
+        style={{
+          height: '500px',
+          margin: '50px 0 30px 0',
+        }}
+        src={image}
+        alt=""
+      />
+      <form className="">
+        <label>
+          Top Text :
+          <input
+            value={toptext}
+            onChange={(event) => {
+              setToptext(event.currentTarget.value);
+            }}
+          />
+        </label>
+
+        <label>
+          Bottom Text:
+          <input
+            value={bottomtext}
+            onChange={(event) => {
+              setBottomtext(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </form>
+      <button
+        className="btn"
+        onClick={() => {
+          setImage(
+            `https://api.memegen.link/images/${drop}/${toptext}/${bottomtext}.png`,
+          );
+        }}
+      >
+        Generate
+      </button>{' '}
+      <button
+        onClick={() => {
+          console.log(image);
+          setImage(image);
+          saveFile();
+        }}
+      >
+        Download
+      </button>
     </div>
   );
 }
+export default App;
